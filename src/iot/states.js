487194432f85096ce61a96aced438a58406ead9d
Bang Dao Tran <bang@eoh.io>
2021-04-06 06:46:36 +0000

    Chore/remove unused code

--- src/iot/states.js
+++
@@ -1,9 +0,0 @@
-import { createGlobalState } from 'react-hooks-global-state';
-
-const { useGlobalState, setGlobalState, getGlobalState } = createGlobalState({
-  configValues: {},
-});
-
-export const useConfigGlobalState = useGlobalState;
-export const setConfigGlobalState = setGlobalState;
-export const getConfigGlobalState = getGlobalState;
