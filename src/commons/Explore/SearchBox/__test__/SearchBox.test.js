0d253a9abdc44f5b14aae9a98dec1ea79c7acfe4
Canh Nguyen <canh.nh@eoh.io>
2021-04-13 14:05:33 +0700

    fix/ep-800-pop-up-parking-input-manually-is-not-message

--- src/commons/Explore/SearchBox/__test__/SearchBox.test.js
+++ src/commons/Explore/SearchBox/__test__/SearchBox.test.js
@@ -1,10 +1,10 @@
 import SearchBox from '../index';
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import { TextInput, TouchableOpacity } from 'react-native';
 import renderer, { act } from 'react-test-renderer';
 
 const mockedNavigate = jest.fn();
-
+const mockedFunc = jest.fn();
 jest.mock('@react-navigation/native', () => {
   return {
     ...jest.requireActual('@react-navigation/native'),
@@ -16,9 +16,11 @@ jest.mock('@react-navigation/native', () => {
 
 describe('Test SearchBox', () => {
   let wrapper;
-  test('SearchBox render', () => {
+  test('SearchBox render isBack=true', () => {
     act(() => {
-      wrapper = renderer.create(<SearchBox isBack={true} />);
+      wrapper = renderer.create(
+        <SearchBox isBack={true} onFocus={mockedFunc} />
+      );
     });
     const instance = wrapper.root;
     const button = instance.findByType(TouchableOpacity);
@@ -26,7 +28,20 @@ describe('Test SearchBox', () => {
     act(() => {
       button.props.onPress();
     });
-    expect(wrapper.toJSON()).toMatchSnapshot();
     expect(mockedNavigate.mock.calls.length).toBe(1);
+    const input = instance.findByType(TextInput);
+
+    act(() => {
+      input.props.onFocus();
+    });
+    expect(mockedFunc.mock.calls.length).toBe(1);
+  });
+  test('SearchBox render isBack=false', () => {
+    act(() => {
+      wrapper = renderer.create(<SearchBox isBack={false} />);
+    });
+    const instance = wrapper.root;
+    const button = instance.findAllByType(TouchableOpacity);
+    expect(button.length).toBe(0);
   });
 });
