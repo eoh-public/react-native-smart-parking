575c14d103e37c45acab7b8405f208a3f8c86cf1
Bang Dao Tran <bang@eoh.io>
2021-04-19 04:03:50 +0000

    Add unittest for search location screen

--- src/screens/MapDashboard/__test__/MapDashboard.test.js
+++ src/screens/MapDashboard/__test__/MapDashboard.test.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import { useIsFocused } from '@react-navigation/native';
+import React, { useState } from 'react';
 import { Provider } from 'react-redux';
 import renderer, { act } from 'react-test-renderer';
 import configureStore from 'redux-mock-store';
+import axios from 'axios';
+import _ from 'lodash';
 
+import { ButtonPopup } from '../../../commons';
+import { API } from '../../../configs';
+import { storeData } from '../../../utils/Storage';
+import ActiveSessionsItem from '../../MyBookingList/components/ActiveSessions/ActiveSessionsItem';
+import ScanningResponsePopup from '../components/ScanningResponsePopup';
 import MapDashboard from '../MapDashboard';
 import SearchBar from '../components/SearchBar';
 
 const mockStore = configureStore([]);
+const mockNavigation = {
+  navigate: jest.fn(),
+};
 
 jest.mock('@react-navigation/native', () => {
   return {
     ...jest.requireActual('@react-navigation/native'),
-    useNavigation: () => ({
-      navigate: jest.fn(),
-    }),
+    useNavigation: () => mockNavigation,
     useIsFocused: jest.fn(),
   };
 });
@@ -25,8 +34,22 @@ jest.mock('react-redux', () => {
   };
 });
 
-describe('Test container MapDashboard', () => {
+const mockSetState = jest.fn();
+const mockRef = { current: { fitToCoordinates: jest.fn() } };
+jest.mock('react', () => {
+  return {
+    ...jest.requireActual('react'),
+    useState: jest.fn((init) => [init, mockSetState]),
+    memo: (x) => x,
+    useRef: () => mockRef,
+  };
+});
+
+jest.mock('axios');
+
+describe('Test MapDashboard', () => {
   let store;
+  let tree;
 
   beforeEach(() => {
     store = mockStore({
@@ -35,14 +58,23 @@ describe('Test container MapDashboard', () => {
         newSavedParking: true,
       },
     });
+    useIsFocused.mockClear();
+    axios.get.mockClear();
   });
+  const activeSessions = {
+    time_remaining: 10,
+    is_paid: true,
+    start_countdown: true,
+    parking: {
+      name: 'Name',
+    },
+  };
 
   test('default render map dashboard show search bar and bottom view', async () => {
     const route = {};
 
-    let tree;
-    act(() => {
-      tree = renderer.create(
+    await act(async () => {
+      tree = await renderer.create(
         <Provider store={store}>
           <MapDashboard route={route} />
         </Provider>
@@ -56,5 +88,185 @@ describe('Test container MapDashboard', () => {
 
     expect(children[0].type.type).toBe(SearchBar.type);
     expect(children[1].props.pointerEvents).toBe('box-none');
+  });
+
+  it('Quick jump to scan', async () => {
+    const route = { params: { scanDataResponse: true } };
+    await act(async () => {
+      tree = await renderer.create(
+        <Provider store={store}>
+          <MapDashboard route={route} />
+        </Provider>
+      );
+    });
+    const scanNodes = tree.root.findAllByType(ScanningResponsePopup);
+    expect(scanNodes).toHaveLength(1);
+  });
+
+  it('active session countdown', async () => {
+    const mockSetMethod = jest.fn();
+    _.range(0, 6).map(() => {
+      useState.mockImplementationOnce((init) => [init, mockSetState]);
+    });
+    useState.mockImplementationOnce((init) => [init, mockSetMethod]);
+    _.range(7, 14).map(() => {
+      useState.mockImplementationOnce((init) => [init, mockSetState]);
+    });
+    useState.mockImplementationOnce((init) => [activeSessions, mockSetState]); // active session
+
+    const route = {};
+    await act(async () => {
+      tree = await renderer.create(
+        <Provider store={store}>
+          <MapDashboard route={route} />
+        </Provider>
+      );
+    });
+
+    expect(mockSetMethod).toBeCalledWith(true);
+  });
+
+  it('active session load cached search', async () => {
+    const mockSetMethod = jest.fn();
+    _.range(0, 7).map(() => {
+      useState.mockImplementationOnce((init) => [init, mockSetState]);
+    });
+    useState.mockImplementationOnce((init) => [init, mockSetMethod]);
+    _.range(8, 14).map(() => {
+      useState.mockImplementationOnce((init) => [init, mockSetState]);
+    });
+    useState.mockImplementationOnce((init) => [activeSessions, mockSetState]); // active session
+
+    const cacheSelectLocation = { latitude: 1, longitude: 1 };
+    await storeData(
+      '@CACHE_SELECT_LOCATION',
+      JSON.stringify(cacheSelectLocation)
+    );
+
+    const route = {};
+    await act(async () => {
+      tree = await renderer.create(
+        <Provider store={store}>
+          <MapDashboard route={route} />
+        </Provider>
+      );
+    });
+
+    expect(mockSetMethod).toBeCalledWith(cacheSelectLocation);
+  });
+
+  it('fetch active session on focus', async () => {
+    useIsFocused.mockImplementation(() => true);
+
+    const route = {};
+    await act(async () => {
+      tree = await renderer.create(
+        <Provider store={store}>
+          <MapDashboard route={route} />
+        </Provider>
+      );
+    });
+
+    expect(axios.get).toBeCalledWith(API.BOOKING.ACTIVE_SESSION, {});
+  });
+
+  it('map is enabled after 0.5s', async () => {
+    const mockSetMethod = jest.fn();
+    _.range(0, 3).map(() => {
+      useState.mockImplementationOnce((init) => [init, mockSetState]);
+    });
+    useState.mockImplementationOnce((init) => [init, mockSetMethod]);
+
+    const route = {};
+    await act(async () => {
+      tree = await renderer.create(
+        <Provider store={store}>
+          <MapDashboard route={route} />
+        </Provider>
+      );
+    });
+
+    expect(mockSetMethod).not.toBeCalled();
+
+    jest.runAllTimers();
+    expect(mockSetMethod).toBeCalledWith(true);
+  });
+
+  it('active session extend', async () => {
+    _.range(0, 14).map(() => {
+      useState.mockImplementationOnce((init) => [init, mockSetState]);
+    });
+    useState.mockImplementationOnce((init) => [activeSessions, mockSetState]); // active session
+
+    const route = {};
+    await act(async () => {
+      tree = await renderer.create(
+        <Provider store={store}>
+          <MapDashboard route={route} />
+        </Provider>
+      );
+    });
+
+    const buttonPopup = tree.root.findAllByType(ButtonPopup)[0];
+
+    act(() => {
+      buttonPopup.props.onPressMain();
+    });
+
+    expect(mockNavigation.navigate).toBeCalled();
+  });
+
+  it('active session hide warning', async () => {
+    const mockSetMethod = jest.fn();
+    _.range(0, 6).map(() => {
+      useState.mockImplementationOnce((init) => [init, mockSetState]);
+    });
+    useState.mockImplementationOnce((init) => [init, mockSetMethod]);
+    _.range(7, 14).map(() => {
+      useState.mockImplementationOnce((init) => [init, mockSetState]);
+    });
+    useState.mockImplementationOnce((init) => [activeSessions, mockSetState]); // active session
+
+    const route = {};
+    await act(async () => {
+      tree = await renderer.create(
+        <Provider store={store}>
+          <MapDashboard route={route} />
+        </Provider>
+      );
+    });
+
+    const buttonPopup = tree.root.findAllByType(ButtonPopup)[0];
+
+    act(() => {
+      buttonPopup.props.onClose();
+    });
+
+    expect(mockSetMethod).toBeCalledWith(false);
+  });
+
+  it('active session on parking complete', async () => {
+    _.range(0, 14).map(() => {
+      useState.mockImplementationOnce((init) => [init, mockSetState]);
+    });
+    useState.mockImplementationOnce((init) => [activeSessions, mockSetState]); // active session
+
+    const route = {};
+    await act(async () => {
+      tree = await renderer.create(
+        <Provider store={store}>
+          <MapDashboard route={route} />
+        </Provider>
+      );
+    });
+
+    const activeSessionItem = tree.root.findByType(ActiveSessionsItem);
+    axios.get.mockClear();
+
+    act(() => {
+      activeSessionItem.props.onParkingCompleted();
+    });
+
+    expect(axios.get).toBeCalledWith(API.BOOKING.ACTIVE_SESSION, {});
   });
 });
--- src/screens/SearchLocation/__test__/index.test.js
+++ src/screens/SearchLocation/__test__/index.test.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import renderer, { act } from 'react-test-renderer';
 import { TextInput } from 'react-native';
 
 import { TESTID } from '../../../configs/Constants';
+import AsyncKeys from '../../../utils/AsyncKey';
+import { deleteData, getData, storeData } from '../../../utils/Storage';
+import SearchBarLocation from '../components/SearchLocation';
 import SearchLocation from '../index';
 
 jest.mock('@react-navigation/native', () => {
@@ -14,6 +18,17 @@ jest.mock('@react-navigation/native', () => {
   };
 });
 
+const mockSetState = jest.fn();
+jest.mock('react', () => {
+  return {
+    ...jest.requireActual('react'),
+    useState: jest.fn((init) => [init, mockSetState]),
+    memo: (x) => x,
+  };
+});
+
+jest.mock('axios');
+
 describe('Test SearchLocation container', () => {
   let tree;
   const route = {
@@ -22,9 +37,14 @@ describe('Test SearchLocation container', () => {
     },
   };
 
-  test('create SearchLocation', () => {
-    act(() => {
-      tree = renderer.create(<SearchLocation route={route} />);
+  beforeEach(async () => {
+    mockSetState.mockClear();
+    axios.get.mockClear();
+  });
+
+  test('create SearchLocation', async () => {
+    await act(async () => {
+      tree = await renderer.create(<SearchLocation route={route} />);
     });
     const instance = tree.root;
     const textInput = instance.findAllByType(TextInput);
@@ -33,5 +53,119 @@ describe('Test SearchLocation container', () => {
     );
 
     expect(searchBar.props.placeholder).toBe('Tôi muốn đỗ xe gần...');
+  });
+
+  it('Load search history not set', async () => {
+    await deleteData(AsyncKeys.RECENT_SEARCH);
+
+    await act(async () => {
+      tree = await renderer.create(<SearchLocation route={route} />);
+    });
+    expect(mockSetState).toBeCalledWith([]);
+  });
+
+  it('Load search history previous', async () => {
+    const searchHistory = [{ this: 'test' }];
+    await storeData(AsyncKeys.RECENT_SEARCH, JSON.stringify(searchHistory));
+
+    await act(async () => {
+      tree = await renderer.create(<SearchLocation route={route} />);
+    });
+    expect(mockSetState).toBeCalledWith(searchHistory);
+  });
+
+  it('search term change', async () => {
+    await act(async () => {
+      tree = await renderer.create(<SearchLocation route={route} />);
+    });
+    axios.get.mockImplementationOnce(() => ({ status: 200 }));
+    expect(axios.get).not.toBeCalled();
+    const searchBar = tree.root.findByType(SearchBarLocation);
+    await act(async () => {
+      searchBar.props.onTextInput('term');
+    });
+    expect(mockSetState).toBeCalledWith('term');
+    expect(axios.get).toBeCalled();
+    expect(mockSetState).toHaveBeenCalledTimes(2);
+  });
+
+  it('search term fail api gmap', async () => {
+    const mockSetSearchData = jest.fn();
+    useState.mockImplementationOnce((init) => [init, mockSetSearchData]);
+    await act(async () => {
+      tree = await renderer.create(<SearchLocation route={route} />);
+    });
+    axios.get.mockImplementationOnce(() => ({ status: 400 }));
+    const searchBar = tree.root.findByType(SearchBarLocation);
+    await act(async () => {
+      searchBar.props.onTextInput('term');
+    });
+    expect(axios.get).toBeCalled();
+    expect(mockSetSearchData).not.toBeCalled();
+  });
+
+  it('search term empty', async () => {
+    await act(async () => {
+      tree = await renderer.create(<SearchLocation route={route} />);
+    });
+    expect(axios.get).not.toBeCalled();
+    const searchBar = tree.root.findByType(SearchBarLocation);
+    await act(async () => {
+      searchBar.props.onTextInput('');
+    });
+    expect(mockSetState).toBeCalledWith('');
+    expect(axios.get).not.toBeCalled();
+  });
+
+  it('save search history normal', async () => {
+    const searchItem = { description: 'description' };
+    const searchData = [searchItem];
+    useState.mockImplementationOnce((init) => [searchData, mockSetState]); // search Data
+    useState.mockImplementationOnce((init) => ['term', mockSetState]); // search input
+    await act(async () => {
+      tree = await renderer.create(<SearchLocation route={route} />);
+    });
+    const searchRow = tree.root.find(
+      (el) => el.props.testID === TESTID.SEARCH_LOCATION_ROW_ITEM
+    );
+    await deleteData(AsyncKeys.RECENT_SEARCH);
+    let recentData = await getData(AsyncKeys.RECENT_SEARCH);
+    expect(recentData).toBeNull();
+
+    await act(async () => {
+      searchRow.props.onPress(searchItem);
+    });
+
+    recentData = await getData(AsyncKeys.RECENT_SEARCH);
+    expect(recentData).not.toBeNull();
+  });
+
+  it('save search history maximum 10', async () => {
+    const searchHistory = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => {
+      return { description: item.toString() };
+    });
+
+    const searchItem = { description: 'description' };
+    const searchData = [searchItem];
+    useState.mockImplementationOnce((init) => [searchData, mockSetState]); // search Data
+    useState.mockImplementationOnce((init) => ['term', mockSetState]); // search input
+    useState.mockImplementationOnce((init) => [init, mockSetState]); // keyboard
+    useState.mockImplementationOnce((init) => [init, mockSetState]); // keyboard
+    useState.mockImplementationOnce((init) => [searchHistory, mockSetState]); // recent data
+
+    await act(async () => {
+      tree = await renderer.create(<SearchLocation route={route} />);
+    });
+    const searchRow = tree.root.find(
+      (el) => el.props.testID === TESTID.SEARCH_LOCATION_ROW_ITEM
+    );
+    await act(async () => {
+      searchRow.props.onPress(searchItem);
+    });
+
+    const recentData = JSON.parse(await getData(AsyncKeys.RECENT_SEARCH));
+    expect(recentData.length).toEqual(10);
+    expect(recentData[0].description).toEqual('description');
+    expect(recentData[9].description).toEqual('9');
   });
 });
