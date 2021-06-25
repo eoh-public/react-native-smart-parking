9d67fbb52d229b1de77c8a7dfd0ea47711173110
Bang Dao Tran <bang@eoh.io>
2021-06-25 06:33:06 +0000

    Resolve EP-1381 "Fix/ fix translation"

--- src/utils/I18n/index.test.js
+++ src/utils/I18n/index.test.js
@@ -1,12 +1,13 @@
-import { setLocale } from './index';
 import i18n from 'i18n-js';
-import i18nTranslate from './index';
+import i18nTranslate, { setLocale } from './index';
 
 test('test setLocale', () => {
+  const language = i18n.locale;
   setLocale('test');
   expect(i18n.locale).toBe('test');
+  setLocale(language);
 });
 
 test('test translate', () => {
-  expect(i18nTranslate('booking_id')).toBe('Booking ID');
+  expect(i18nTranslate('booking_id')).toBe('ID đặt chỗ');
 });
